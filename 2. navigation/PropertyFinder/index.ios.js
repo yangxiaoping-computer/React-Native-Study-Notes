938ed64e75f3c2a8c504a38a3d3b94d602fa0d5c
yangxiaoping <yangxiaoping@ihealthlabs.com.cn>
2016-03-11 18:57:08 +0800

    add navigation

---
+++ 2. navigation/PropertyFinder/index.ios.js
@@ -0,0 +1,35 @@
+'use strict';
+var React = require('react-native');
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container:{
+    flex:1
+  }
+});
+
+class HelloWorld extends React.Component {
+  render() {
+    //return React.createElement(React.Text, {style: styles.text}, "Hello World!");
+    return <React.Text style={styles.text}> Hello World (Again)</React.Text>;
+  }
+}
+
+class PropertyFinderApp extends React.Component{
+  render(){
+    return(
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Proerty Finder',
+          component: HelloWorld,
+        }}/>
+      );
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
