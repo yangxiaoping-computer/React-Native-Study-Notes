c7718d9cabac59b2b2a5e69edfd143118aa18ecb
yangxiaoping <yangxiaoping@ihealthlabs.com.cn>
2016-03-11 18:22:23 +0800

    hello Word

---
+++ 1. Hello World/PropertyFinder/index.ios.js
@@ -0,0 +1,19 @@
+'use strict';
+var React = require('react-native');
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    //return React.createElement(React.Text, {style: styles.text}, "Hello World!");
+    return <React.Text style={styles.text}> Hello World (Again)</React.Text>;
+  }
+}
+
+React.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
